10788a7392af47a972ceb1adece042852262562e
ntt123 <ntt12@home>
2022-12-18 00:47:01 +0800

    add pytest

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ python3 -m venv "${VENV_DIR}"
 source "${VENV_DIR}/bin/activate"
 python --version
 pip install --upgrade pip
-pip install black==22.3.0 pylint==2.15.8 pytype==2022.12.15
+pip install black==22.3.0 pylint==2.15.8 pytype==2022.12.15 pytest==7.2.0
 pip install -r requirements.txt
 black --diff --check $(git ls-files '*.py')
 pylint --disable=all --enable=unused-import $(git ls-files '*.py')
